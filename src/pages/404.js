2426cff028bd1af41df6689e535a95fd843c285e
Tom Billington <tombill143@gmail.com>
2023-05-13 15:35:35 +0200

    making vercel work

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,7 +12,7 @@ const Notfound = () => {
 
   return (
     <div className="not-found">
-      <h1>Oooops...</h1>
+      <h1>Oooops....</h1>
       <h2>That page cannot be found.</h2>
       <p>
         Go back to the <Link href="/">Homepage</Link>
