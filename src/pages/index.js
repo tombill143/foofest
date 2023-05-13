3257b41ddeab5ed3dcf064f556bd38457eae7adb
Marie Isaksen <mari17ry@stud.kea.dk>
2023-05-13 14:18:07 +0200

    first test and commit on 3rd mariebranch lol

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1>Homepage testing on marie branch!</h1>
     </div>
   );
 }
