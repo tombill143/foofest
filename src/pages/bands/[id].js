9445a4f956dc2d1808b2fdcdb37b4b52c613b999
Tom Billington <tombill143@gmail.com>
2023-05-13 14:44:35 +0200

    ran npm run build and made some pages

--- src/pages/bands/[id].js
+++ src/pages/bands/[id].js
@@ -1,7 +1,51 @@
-const Bio = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch("https://positive-pushy-oatmeal.glitch.me/bands");
+  const data = await res.json();
+
+  const paths = data.map((band) => {
+    return {
+      params: { id: band.slug ? band.slug.toString() : "" },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(
+    "https://positive-pushy-oatmeal.glitch.me/bands/" + id
+  );
+  const data = await res.json();
+
+  return {
+    props: { band: data },
+  };
+};
+
+const Bio = ({ band }) => {
+  let members = band.members;
+  if (members && typeof members === "string") {
+    members = members.split(",");
+  }
+
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{band.name}</h1>
+      {members && (
+        <ul>
+          {members.map((member) => (
+            <li key={member}>{member.trim()}</li>
+          ))}
+        </ul>
+      )}
+      <p>{band.genre}</p>
+      <p>{band.logoCredits}</p>
+      <p>{band.logo}</p>
+      <p>{band.bio}</p>
     </div>
   );
 };
