a731601d24b2c25f84aefe8d46936fc7a133b50d
Marie Isaksen <mari17ry@stud.kea.dk>
2023-05-13 15:22:49 +0200

    added our logo and changed to white

--- src/pages/componants/Navbar.js
+++ src/pages/componants/Navbar.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "/src/styles/Home.module.css";
 
 const Navbar = () => {
   return (
     <nav>
       <div class="logo">
-        <Image src="/logo.png" alt="picture" width={128} height={100} />
+        <Image src="/newlogo.svg" className={styles.logocolor} alt="picture" width={128} height={100} />
+        {/* <Image src="/logo.png" alt="picture" width={128} height={100} /> */}
       </div>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
