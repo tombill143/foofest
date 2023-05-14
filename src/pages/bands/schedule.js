e8915e37a7bcfaf87f9adc139a10358df87cf3a3
Marie Isaksen <mari17ry@stud.kea.dk>
2023-05-14 13:01:20 +0200

    data shows, but the wrong bands for the wrong time

--- src/pages/bands/schedule.js
+++ src/pages/bands/schedule.js
@@ -18,14 +18,118 @@ const BandsSchedule = ({ bands }) => {
   return (
     <div>
       <h1>Schedule</h1>
-      {bands.mon && bands.mon.map((band, index) => (
-        <div>
-        <h3 key={index}>{band.act}</h3>
 
+{/* midhard */}
+      <div>
         <h2>Midgard</h2>
-        </div>
+        <h3>Monday</h3>
 
-      ))}
+        {bands.Midgard && bands.Midgard.mon && bands.Midgard.mon.map((band) => (
+         <div key={`${band.act}-${band.start}-${band.end}`}>
+            {/* dorsnt change anything, still the wrong data at the wrng time stamps */}
+            <p>Start: {band.start}</p>
+            <p>End: {band.end}</p>
+            <p>Act: {band.act}</p>
+          </div>
+        ))}
+
+       <h3>tuesday</h3>
+
+       {bands.Midgard && bands.Midgard.tue && bands.Midgard.tue.map((band) => (
+          <div key={band.act}>
+          <p>Start: {band.start}</p>
+          <p>End: {band.end}</p>
+          <p>Act: {band.act}</p>
+         </div>
+         ))}
+
+<h3>Wednesday</h3>
+
+{bands.Midgard && bands.Midgard.wed && bands.Midgard.wed.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+
+
+
+<h3>Thursday</h3>
+
+{bands.Midgard && bands.Midgard.thu && bands.Midgard.thu.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+
+
+<h3>Friday</h3>
+
+{bands.Midgard && bands.Midgard.fri && bands.Midgard.fri.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+
+
+<h3>Saturday</h3>
+
+{bands.Midgard && bands.Midgard.sat && bands.Midgard.sat.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+
+
+
+<h3>Sunday</h3>
+
+{bands.Midgard && bands.Midgard.sun && bands.Midgard.sun.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+      </div>
+
+
+
+
+
+{/* jotunheim */}
+
+      <div>
+        <h2>Jotunheim</h2>
+        <h3>Monday</h3>
+        {bands.Jotunheim && bands.Jotunheim.mon && bands.Jotunheim.mon.map((band) => (
+          <div key={band.act}>
+            <p>Start: {band.start}</p>
+            <p>End: {band.end}</p>
+            <p>Act: {band.act}</p>
+          </div>
+        ))}
+      </div>
+
+   {/* vanaheim */}
+      <div>
+        <h2>Vanaheim</h2>
+        {bands.Vanaheim && bands.Vanaheim.mon && bands.Vanaheim.mon.map((band) => (
+          <div key={band.act}>
+            <p>Start: {band.start}</p>
+            <p>End: {band.end}</p>
+            <p>Act: {band.act}</p>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 };
