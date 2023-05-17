483ce9570c06c4239e71f4b5f6f32e83a250f823
Marie Isaksen <mari17ry@stud.kea.dk>
2023-05-17 14:33:18 +0200

    redesigned the navbar with flex and stuff

--- src/pages/componants/Navbar.js
+++ src/pages/componants/Navbar.js
@@ -5,13 +5,19 @@ import styles from "/src/styles/Home.module.css";
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">
+      <div class="logo">
+        <Link href="/">
         <Image src="/newlogo.svg" className={styles.logocolor} alt="picture" width={128} height={100} />
         {/* <Image src="/logo.png" alt="picture" width={128} height={100} /> */}
+        </Link>
       </div>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/bands">Band Listings</Link>
+      <ul>
+      <Link href="/">HOME</Link>
+      <Link href="/bands">LINEUP</Link>
+      <Link href="/schedule">SCHEDULE</Link>
+      <Link href="/about">ABOUT</Link>
+      <Link className={styles.btn} href="/booking"> BOOK</Link>
+      </ul>
     </nav>
   );
 };
