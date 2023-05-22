08ce9cb4188ed53d73f02782ea8a4d7847df3a14
Tom Billington <tombill143@gmail.com>
2023-05-22 12:08:32 +0200

    cant remember what i did here

---
+++ src/pages/bands/schedule.js
@@ -0,0 +1,143 @@
+import React from "react";
+import styles from "../../styles/Bands.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://positive-pushy-oatmeal.glitch.me/schedule");
+  const data = await res.json();
+
+  return {
+    props: {
+      bands: data,
+    },
+  };
+};
+
+const BandsSchedule = ({ bands }) => {
+  console.log("Bands:", bands);
+
+  return (
+    <div>
+      <h1>Schedule</h1>
+
+{/* midhard */}
+      <div>
+        <h2>Midgard</h2>
+        <h3>Monday</h3>
+
+        {bands.Midgard && bands.Midgard.mon && bands.Midgard.mon.map((band) => (
+         <div key={band.act}>
+
+{/* to change the cosnole error, chat gpt gives this fix:
+
+<div key={`${band.act}-${band.start}-${band.end}-${band.act === "break" ? "break" : "" }`} >
+as the issue is thay the 'act's 'break' cant have individual keys, as their 'act' is called the same. If it becomes an issue, try this
+            */}
+
+            <p>Start: {band.start}</p>
+            <p>End: {band.end}</p>
+            <p>Act: {band.act}</p>
+          </div>
+        ))}
+
+       <h3>tuesday</h3>
+
+       {bands.Midgard && bands.Midgard.tue && bands.Midgard.tue.map((band) => (
+          <div key={band.act}>
+          <p>Start: {band.start}</p>
+          <p>End: {band.end}</p>
+          <p>Act: {band.act}</p>
+         </div>
+         ))}
+
+<h3>Wednesday</h3>
+
+{bands.Midgard && bands.Midgard.wed && bands.Midgard.wed.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+
+
+
+<h3>Thursday</h3>
+
+{bands.Midgard && bands.Midgard.thu && bands.Midgard.thu.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+
+
+<h3>Friday</h3>
+
+{bands.Midgard && bands.Midgard.fri && bands.Midgard.fri.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+
+
+<h3>Saturday</h3>
+
+{bands.Midgard && bands.Midgard.sat && bands.Midgard.sat.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+
+
+
+<h3>Sunday</h3>
+
+{bands.Midgard && bands.Midgard.sun && bands.Midgard.sun.map((band) => (
+   <div key={band.act}>
+   <p>Start: {band.start}</p>
+   <p>End: {band.end}</p>
+   <p>Act: {band.act}</p>
+  </div>
+  ))}
+      </div>
+
+
+
+
+
+{/* jotunheim */}
+
+      <div>
+        <h2>Jotunheim</h2>
+        <h3>Monday</h3>
+        {bands.Jotunheim && bands.Jotunheim.mon && bands.Jotunheim.mon.map((band) => (
+          <div key={band.act}>
+            <p>Start: {band.start}</p>
+            <p>End: {band.end}</p>
+            <p>Act: {band.act}</p>
+          </div>
+        ))}
+      </div>
+
+   {/* vanaheim */}
+      <div>
+        <h2>Vanaheim</h2>
+        {bands.Vanaheim && bands.Vanaheim.mon && bands.Vanaheim.mon.map((band) => (
+          <div key={band.act}>
+            <p>Start: {band.start}</p>
+            <p>End: {band.end}</p>
+            <p>Act: {band.act}</p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default BandsSchedule;
