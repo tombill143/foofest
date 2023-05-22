a93fa34f47ce8bd4098c8dae38e7bf1f8b14c1d8
Tom Billington <tombill143@gmail.com>
2023-05-22 15:04:38 +0200

    made buyers page and selection page

---
+++ src/pages/booking/buyers_info.js
@@ -0,0 +1,105 @@
+import React from "react";
+import styles from "../../styles/Booking.module.css";
+import Link from "next/link";
+import { useState } from "react";
+
+const BuyersInfo = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    zipcode: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission or validation here
+    console.log(formData); // Example: Logging form data to the console
+  };
+
+  return (
+    <div className={styles.gridContainer}>
+      <section className={styles.home_hero}>
+        <div className={styles.leftColumn}>
+          <img
+            src="/selectionimg.JPG"
+            alt="Description of the image"
+            className={styles.image}
+          />
+        </div>
+      </section>
+      <section className={styles.home_hero}>
+        <div className={styles.rightColumn}>
+          <div className={styles.checkboxContainer}>
+            {/* Existing code for checkboxes and other elements */}
+            <h2 className={styles.h2buyersInfo}>Buyers Info</h2>
+
+            <form onSubmit={handleSubmit} className={styles.formContainer}>
+              <label>
+                First Name:
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  className={styles.formInput}
+                />
+              </label>
+              <label>
+                Last Name:
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  className={styles.formInput}
+                />
+              </label>
+              <label>
+                Address:
+                <input
+                  type="text"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleChange}
+                  className={styles.formInput}
+                />
+              </label>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className={styles.formInput}
+                />
+              </label>
+              <label className={styles.zipcodeLabel}>
+                Zipcode:
+                <input
+                  type="text"
+                  name="zipcode"
+                  value={formData.zipcode}
+                  onChange={handleChange}
+                  className={styles.formInput}
+                />
+              </label>
+
+              <button type="submit" className={styles.nextButton}>
+                Next
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default BuyersInfo;
