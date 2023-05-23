3cf159156ff34c368c07ec4e768a0acf30ccad2c
Marie Isaksen <mari17ry@stud.kea.dk>
2023-05-23 16:26:31 +0200

    working on showing .jpg and .png img on [id]

--- src/pages/bands/[id].js
+++ src/pages/bands/[id].js
@@ -1,3 +1,5 @@
+import styles from "../../styles/Bands.module.css";
+
 export const getStaticPaths = async () => {
   const res = await fetch("https://positive-pushy-oatmeal.glitch.me/bands");
   const data = await res.json();
@@ -26,6 +28,14 @@ export const getStaticProps = async (context) => {
   };
 };
 
+
+
+function BandImages(image){
+
+}
+
+
+
 const Bio = ({ band }) => {
   let members = band.members;
   if (members && typeof members === "string") {
@@ -33,18 +43,27 @@ const Bio = ({ band }) => {
   }
 
   return (
-    <div>
+    <div className={styles.all_container} >
       <h1>{band.name}</h1>
+
+      
+
+      <img src={band.logo} alt="band image" />
+      {/* the image */}
+
       {members && (
-        <ul>
+        <ul className={styles.band_members}>
           {members.map((member) => (
             <li key={member}>{member.trim()}</li>
           ))}
         </ul>
       )}
+
       <p>{band.genre}</p>
       <p>{band.logoCredits}</p>
-      <p>{band.logo}</p>
+      {/* the weird striing of text */}
+
+
       <p>{band.bio}</p>
     </div>
   );
