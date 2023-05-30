93a8d9077ce4bf8495a229b034f25d7aeb1e732c
Tom Billington <tombill143@gmail.com>
2023-05-30 19:23:38 +0200

    finally sorted the styling for the tents

--- src/pages/booking/selection.js
+++ src/pages/booking/selection.js
@@ -8,7 +8,6 @@ const Selection = () => {
   const { ticketType, numTickets } = router.query;
 
   const handleNext = () => {
-    // Replace "destination-page" with the actual page where you want to navigate to
     const destinationPage = `/booking/destination-page`;
 
     // Prepare the query parameters to pass to the destination page
@@ -53,13 +52,41 @@ const Selection = () => {
                 <input type="radio" name="ticket" value="vip" />
                 VIP Ticket - 1299;
               </label>
-              <h2 className={styles.choosingTent}>Choose Tents</h2>
+
+              <h2 className={styles.choosingTent}>Number of Tickets</h2>
               <hr className={styles.hrLine} />
               <select className={styles.dropdown}>
-                <option value="tent1">Tent 1</option>
-                <option value="tent2">Tent 2</option>
-                <option value="tent3">Tent 3</option>
+                <option value="ticket1">1</option>
+                <option value="ticket2">2</option>
+                <option value="ticket3">3</option>
+                <option value="ticket4">4</option>
+                <option value="ticket5">5</option>
+                <option value="ticket6">6</option>
+                <option value="ticket7">7</option>
+                <option value="ticket8">8</option>
+                <option value="ticket9">9</option>
+                <option value="ticket10">10</option>
               </select>
+              <h2 className={styles.choosingTent}>Choose Tents</h2>
+              <hr className={styles.hrLine} />
+              <div className={styles.columnContainer}>
+                <div>
+                  <h3 className={styles.choosingTent}>2 Man Tent</h3>
+                  <select className={styles.dropdown}>
+                    <option value="tent1">Tent 1</option>
+                    <option value="tent2">Tent 2</option>
+                    <option value="tent3">Tent 3</option>
+                  </select>
+                </div>
+                <div>
+                  <h3 className={styles.choosingTent}>3 Man Tent</h3>
+                  <select className={styles.dropdown}>
+                    <option value="tent1">Tent 1</option>
+                    <option value="tent2">Tent 2</option>
+                    <option value="tent3">Tent 3</option>
+                  </select>
+                </div>
+              </div>
               <h2 className={styles.choosingTent}>Optionals</h2>
               <hr className={styles.hrLine} />
               <label>
