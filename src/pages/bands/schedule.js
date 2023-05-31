1dbc49bff8cd4b9f1b4dfa33fae857d59672c62e
Marie Isaksen <mari17ry@stud.kea.dk>
2023-05-31 17:01:42 +0200

    schedule working and styled!

--- src/pages/bands/schedule.js
+++ src/pages/bands/schedule.js
@@ -16,128 +16,357 @@ const BandsSchedule = ({ bands }) => {
   console.log("Bands:", bands);
 
   return (
-    <div>
+    <div className={styles.all_container}>
       <h1>Schedule</h1>
 
-{/* midhard */}
-      <div>
-        <h2>Midgard</h2>
-        <h3>Monday</h3>
+<div className={styles.day_schedule}>
+<h2>MONDAY</h2>
+<table className={styles.schedule_table}>
+  {/* <thead className={styles.thead_schedule}>
 
-        {bands.Midgard && bands.Midgard.mon && bands.Midgard.mon.map((band) => (
-         <div key={band.act}>
+       
+          <h3>MIDGARD</h3>
+          <h3>JOTUNHEIM</h3>
+          <h3>VANAHEIM</h3>
+        
+        </thead> */}
 
-{/* to change the cosnole error, chat gpt gives this fix:
-
-<div key={`${band.act}-${band.start}-${band.end}-${band.act === "break" ? "break" : "" }`} >
-as the issue is thay the 'act's 'break' cant have individual keys, as their 'act' is called the same. If it becomes an issue, try this
-            */}
-
-            <p>Start: {band.start}</p>
-            <p>End: {band.end}</p>
-            <p>Act: {band.act}</p>
-          </div>
+        <tbody className={styles.tbody_schedule}>
+        <tr className={styles.tr}>
+          <td className={styles.td}>
+           <h3>MIDGARD</h3>
+          {bands.Midgard && bands.Midgard.mon && bands.Midgard.mon.map((band) => (
+           <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
         ))}
+          </td>
 
-       <h3>tuesday</h3>
+          <td className={styles.td}>
+          <h3>JOTUNHEIM</h3>
 
-       {bands.Midgard && bands.Midgard.tue && bands.Midgard.tue.map((band) => (
-          <div key={band.act}>
-          <p>Start: {band.start}</p>
-          <p>End: {band.end}</p>
-          <p>Act: {band.act}</p>
-         </div>
-         ))}
+          {bands.Jotunheim && bands.Jotunheim.mon && bands.Jotunheim.mon.map((band) => (
+             <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td> 
 
-<h3>Wednesday</h3>
+          <td className={styles.td}>
+          <h3>VANAHEIM</h3>
 
-{bands.Midgard && bands.Midgard.wed && bands.Midgard.wed.map((band) => (
-   <div key={band.act}>
-   <p>Start: {band.start}</p>
-   <p>End: {band.end}</p>
-   <p>Act: {band.act}</p>
-  </div>
-  ))}
+          {bands.Vanaheim && bands.Vanaheim.mon && bands.Vanaheim.mon.map((band) => (
+         <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+         <p><strong> {band.act}</strong></p>
+         <p>{band.start} - {band.end}</p>
+         
+       </div>
+        ))}
+          </td> 
+        </tr>
+        </tbody>
+      </table> 
+      </div>
+      
+ 
+<div className={styles.day_schedule}>
+<h2>TUESDAY</h2>
+<table className={styles.schedule_table}>
+
+        <tbody className={styles.tbody_schedule}>
+        <tr className={styles.tr}>
+          <td className={styles.td}>
+           <h3>MIDGARD</h3>
+          {bands.Midgard && bands.Midgard.tue && bands.Midgard.tue.map((band) => (
+           <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td>
+
+          <td className={styles.td}>
+          <h3>JOTUNHEIM</h3>
+
+          {bands.Jotunheim && bands.Jotunheim.tue && bands.Jotunheim.tue.map((band) => (
+             <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td> 
+
+          <td className={styles.td}>
+          <h3>VANAHEIM</h3>
+
+          {bands.Vanaheim && bands.Vanaheim.tue && bands.Vanaheim.tue.map((band) => (
+         <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+         <p><strong> {band.act}</strong></p>
+         <p>{band.start} - {band.end}</p>
+         
+       </div>
+        ))}
+          </td> 
+        </tr>
+        </tbody>
+      </table> 
+      </div>
+      
+      
+     
+
+   
+<div className={styles.day_schedule}>
+<h2>WEDNESDAY</h2>
+<table className={styles.schedule_table}>
+
+        <tbody className={styles.tbody_schedule}>
+        <tr className={styles.tr}>
+          <td className={styles.td}>
+           <h3>MIDGARD</h3>
+          {bands.Midgard && bands.Midgard.wed && bands.Midgard.wed.map((band) => (
+           <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td>
+
+          <td className={styles.td}>
+          <h3>JOTUNHEIM</h3>
+
+          {bands.Jotunheim && bands.Jotunheim.wed && bands.Jotunheim.wed.map((band) => (
+             <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td> 
+
+          <td className={styles.td}>
+          <h3>VANAHEIM</h3>
+
+          {bands.Vanaheim && bands.Vanaheim.wed && bands.Vanaheim.wed.map((band) => (
+         <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+         <p><strong> {band.act}</strong></p>
+         <p>{band.start} - {band.end}</p>
+         
+       </div>
+        ))}
+          </td> 
+        </tr>
+        </tbody>
+      </table> 
+      </div>
 
 
 
-<h3>Thursday</h3>
 
-{bands.Midgard && bands.Midgard.thu && bands.Midgard.thu.map((band) => (
-   <div key={band.act}>
-   <p>Start: {band.start}</p>
-   <p>End: {band.end}</p>
-   <p>Act: {band.act}</p>
-  </div>
-  ))}
+<div className={styles.day_schedule}>
+<h2>THURSDAY</h2>
+<table className={styles.schedule_table}>
+
+        <tbody className={styles.tbody_schedule}>
+        <tr className={styles.tr}>
+          <td className={styles.td}>
+           <h3>MIDGARD</h3>
+          {bands.Midgard && bands.Midgard.thu && bands.Midgard.thu.map((band) => (
+           <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td>
+
+          <td className={styles.td}>
+          <h3>JOTUNHEIM</h3>
+
+          {bands.Jotunheim && bands.Jotunheim.thu && bands.Jotunheim.thu.map((band) => (
+             <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td> 
+
+          <td className={styles.td}>
+          <h3>VANAHEIM</h3>
+
+          {bands.Vanaheim && bands.Vanaheim.thu && bands.Vanaheim.thu.map((band) => (
+         <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+         <p><strong> {band.act}</strong></p>
+         <p>{band.start} - {band.end}</p>
+         
+       </div>
+        ))}
+          </td> 
+        </tr>
+        </tbody>
+      </table> 
+      </div>
 
 
-<h3>Friday</h3>
-
-{bands.Midgard && bands.Midgard.fri && bands.Midgard.fri.map((band) => (
-   <div key={band.act}>
-   <p>Start: {band.start}</p>
-   <p>End: {band.end}</p>
-   <p>Act: {band.act}</p>
-  </div>
-  ))}
 
 
-<h3>Saturday</h3>
+ <div className={styles.day_schedule}>
+ <h2>FRIDAY</h2>
+ <table className={styles.schedule_table}>
 
-{bands.Midgard && bands.Midgard.sat && bands.Midgard.sat.map((band) => (
-   <div key={band.act}>
-   <p>Start: {band.start}</p>
-   <p>End: {band.end}</p>
-   <p>Act: {band.act}</p>
-  </div>
-  ))}
+        <tbody className={styles.tbody_schedule}>
+        <tr className={styles.tr}>
+          <td className={styles.td}>
+           <h3>MIDGARD</h3>
+          {bands.Midgard && bands.Midgard.fri && bands.Midgard.fri.map((band) => (
+           <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td>
+
+          <td className={styles.td}>
+          <h3>JOTUNHEIM</h3>
+
+          {bands.Jotunheim && bands.Jotunheim.fri && bands.Jotunheim.fri.map((band) => (
+             <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td> 
+
+          <td className={styles.td}>
+          <h3>VANAHEIM</h3>
+
+          {bands.Vanaheim && bands.Vanaheim.fri && bands.Vanaheim.fri.map((band) => (
+         <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+         <p><strong> {band.act}</strong></p>
+         <p>{band.start} - {band.end}</p>
+         
+       </div>
+        ))}
+          </td> 
+        </tr>
+        </tbody>
+      </table> 
+      </div>
 
 
+ 
 
-<h3>Sunday</h3>
+<div className={styles.day_schedule}>
+<h2>SATURDAY</h2>
+<table className={styles.schedule_table}>
 
-{bands.Midgard && bands.Midgard.sun && bands.Midgard.sun.map((band) => (
-   <div key={band.act}>
-   <p>Start: {band.start}</p>
-   <p>End: {band.end}</p>
-   <p>Act: {band.act}</p>
-  </div>
-  ))}
+        <tbody className={styles.tbody_schedule}>
+        <tr className={styles.tr}>
+          <td className={styles.td}>
+           <h3>MIDGARD</h3>
+          {bands.Midgard && bands.Midgard.sat && bands.Midgard.sat.map((band) => (
+           <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td>
+
+          <td className={styles.td}>
+          <h3>JOTUNHEIM</h3>
+
+          {bands.Jotunheim && bands.Jotunheim.sat && bands.Jotunheim.sat.map((band) => (
+             <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td> 
+
+          <td className={styles.td}>
+          <h3>VANAHEIM</h3>
+
+          {bands.Vanaheim && bands.Vanaheim.sat && bands.Vanaheim.sat.map((band) => (
+         <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+         <p><strong> {band.act}</strong></p>
+         <p>{band.start} - {band.end}</p>
+         
+       </div>
+        ))}
+          </td> 
+        </tr>
+        </tbody>
+      </table> 
       </div>
 
 
 
 
 
-{/* jotunheim */}
+<div className={styles.day_schedule}>
+<h2>SUNDAY</h2>
+<table className={styles.schedule_table}>
 
-      <div>
-        <h2>Jotunheim</h2>
-        <h3>Monday</h3>
-        {bands.Jotunheim && bands.Jotunheim.mon && bands.Jotunheim.mon.map((band) => (
-          <div key={band.act}>
-            <p>Start: {band.start}</p>
-            <p>End: {band.end}</p>
-            <p>Act: {band.act}</p>
-          </div>
+        <tbody className={styles.tbody_schedule}>
+        <tr className={styles.tr}>
+          <td className={styles.td}>
+           <h3>MIDGARD</h3>
+          {bands.Midgard && bands.Midgard.sun && bands.Midgard.sun.map((band) => (
+           <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
         ))}
+          </td>
+
+          <td className={styles.td}>
+          <h3>JOTUNHEIM</h3>
+
+          {bands.Jotunheim && bands.Jotunheim.sun && bands.Jotunheim.sun.map((band) => (
+             <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+             <p><strong> {band.act}</strong></p>
+             <p>{band.start} - {band.end}</p>
+             
+           </div>
+        ))}
+          </td> 
+
+          <td className={styles.td}>
+          <h3>VANAHEIM</h3>
+
+          {bands.Vanaheim && bands.Vanaheim.sun && bands.Vanaheim.sun.map((band) => (
+         <div className={`${styles.each_band} ${band.act === 'break' ? styles.break_act : ''}`} key={`${band.act}-${band.start}-${band.end}`}>
+         <p><strong> {band.act}</strong></p>
+         <p>{band.start} - {band.end}</p>
+         
+       </div>
+        ))}
+          </td> 
+        </tr>
+        </tbody>
+      </table> 
       </div>
 
-   {/* vanaheim */}
-      <div>
-        <h2>Vanaheim</h2>
-        {bands.Vanaheim && bands.Vanaheim.mon && bands.Vanaheim.mon.map((band) => (
-          <div key={band.act}>
-            <p>Start: {band.start}</p>
-            <p>End: {band.end}</p>
-            <p>Act: {band.act}</p>
-          </div>
-        ))}
-      </div>
 
-    </div>
+
+      </div>
+      
+
   );
 };
 
 export default BandsSchedule;
+
